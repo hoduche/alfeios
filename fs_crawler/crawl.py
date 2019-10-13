ab5da582db0d272c17c32754d0c3383220bed5e2
Henri-Olivier Duché <hoduche@yahoo.fr>
2019-10-13 18:47:26 +0200

    add docstrings

--- fs_crawler/crawl.py
+++ fs_crawler/crawl.py
@@ -2,16 +2,29 @@ import pandas as pd
 
 
 def parse(path):
+    """
+    Recursively crawl through a root folder to list all its files
+    (with their parent folder and size)
+
+    :param path: path of the root folder to parse
+    :type path: pathlib.Path
+
+    :return: DataFrame with one row per file and columns:
+             * Folder: path of the file's parent folder - pathlib.Path
+             * File: filename - str
+             * Size: size of the file in bytes - unsigned int
+    :rtype: pandas.DataFrame
+    """
     listing = list()
-    recursive_parse(path, listing)
+    _recursive_parse(path, listing)
     result = pd.DataFrame(listing, columns=['Folder', 'File', 'Size'])
     return result
 
 
-def recursive_parse(path, listing):
+def _recursive_parse(path, listing):
     for each in path.iterdir():
         if each.is_dir():
-            recursive_parse(each, listing)
+            _recursive_parse(each, listing)
         elif each.is_file():
             folder = each.resolve().expanduser().parent
             file = each.name
@@ -21,5 +34,11 @@ def recursive_parse(path, listing):
 
 
 def dump_folder_content(path):
+    """
+    Dump root folder files listing in a csv file
+
+    :param path: path of the root folder to parse
+    :type path: pathlib.Path
+    """
     listing = parse(path)
     listing.to_csv(str(path) + '_listing.csv', index=False)
