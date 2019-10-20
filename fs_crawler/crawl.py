fab1331fe97a0fe38f073a44bee93bee90a25ac0
Henri-Olivier Duché <hoduche@yahoo.fr>
2019-10-20 15:56:22 +0200

    hash content and use this as dict key

--- fs_crawler/crawl.py
+++ fs_crawler/crawl.py
@@ -1,4 +1,12 @@
+import collections
+import hashlib
+import os
+import pathlib
+
 import pandas as pd
+
+MAX_SIZE = 1048576  # ie  1 Mo
+BLOCK_SIZE = 65536  # ie 64 Ko
 
 
 def parse(path):
@@ -42,3 +50,55 @@ def dump_folder_content(path):
     """
     listing = parse(path)
     listing.to_csv(str(path) + '_listing.csv', index=False)
+
+
+def main(path):
+    content_listing = collections.defaultdict(list)
+    dirs_listing = {}
+
+    for dir_path, subdir_names, file_names in os.walk(path, topdown=False):
+        key_set = set()
+
+        for file_name in file_names:
+            file_path = pathlib.Path(dir_path) / file_name
+            file_hasher = hashlib.md5()
+            with open(file_path, 'rb') as file_content:
+                content_stream = file_content.read(BLOCK_SIZE)
+                while len(content_stream) > 0:
+                    file_hasher.update(content_stream)
+                    content_stream = file_content.read(BLOCK_SIZE)
+            file_content_key = file_hasher.hexdigest()
+            content_listing[file_content_key].append(str(file_path))
+            key_set.add(file_content_key)
+
+        for subdir_name in subdir_names:
+            subdir_path = pathlib.Path(dir_path) / subdir_name
+            key_set.add(dirs_listing[str(subdir_path)])
+
+        if key_set:
+            dir_content = str(key_set).encode()
+            dir_content_key = hashlib.md5(dir_content).hexdigest()
+            content_listing[dir_content_key].append(str(dir_path))
+            dirs_listing[str(dir_path)] = dir_content_key
+
+    return content_listing, dirs_listing
+
+
+if __name__ == '__main__':
+    tests_data_path = pathlib.Path(__file__).resolve().expanduser()
+    tests_data_path = tests_data_path.parent.parent / 'tests' / 'data'
+    content_listing, dirs_listing = main(tests_data_path / 'Folder0')
+    print('------------------')
+    tab = '    '
+    for x in content_listing:
+        print(tab + x)
+        for y in content_listing[x]:
+            print(2 * tab + y)
+    print('------------------')
+    for x in dirs_listing:
+        print(tab + x)
+        print(2 * tab + dirs_listing[x])
+    print('------------------')
+
+
+# todo : rajouter le getsize sur les fichiers (plutôt les contenus) + séparer les files des folders (2 dict ou un tuple dans le dict)
