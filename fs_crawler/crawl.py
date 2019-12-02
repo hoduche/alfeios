552cebabbadfbea1877ecca3667a54592aa55773
Henri-Olivier Duché <hoduche@yahoo.fr>
2019-12-03 00:10:09 +0100

    add unify function

--- fs_crawler/crawl.py
+++ fs_crawler/crawl.py
@@ -9,11 +9,11 @@ FILE_TYPE = 'FILE'
 DIR_TYPE = 'DIR'
 
 
-def crawl(path, json_listing_path=None, json_tree_path=None, exclusion=[]):
+def crawl(path, exclusion=None):
     """
     Recursively crawls through a root directory to list its content.
     It manages two data structures:
-    listing : a collections.defaultdict(list) whose keys are tuples (hash, type, size)
+    listing : a collections.defaultdict(set) whose keys are tuples (hash, type, size)
               and values are list of pathlib.Path
     tree    : a dictionary whose keys are pathlib.Path and values are tuples (hash, type, size)
     in both data structures, the type distinguishes files from directories
@@ -21,82 +21,120 @@ def crawl(path, json_listing_path=None, json_tree_path=None, exclusion=[]):
     :param path: path of the root directory to parse
     :type path: pathlib.Path
 
-    :param json_listing_path: path to an existing json serialized listing to append to
-    :type json_listing_path: pathlib.Path
-
-    :param json_tree_path: path to an existing json serialized tree to append to
-    :type json_tree_path: pathlib.Path
-
     :param exclusion: list of directories and files not to parse
-    :type exclusion: list of pathlib.Path
+    :type exclusion: list of str
 
-    :return: root directory listing and tree
-    :rtype: collections.defaultdict(list), dict
+    :return: root directory listing
+    :rtype: collections.defaultdict(set) = {(hash, type, int): {pathlib.Path}}
+
+    :return: root directory tree
+    :rtype: dict = {pathlib.Path: (hash, type, int)}
     """
-    if json_listing_path:
-        listing = load_json_listing(json_listing_path)
-    else:
-        listing = collections.defaultdict(list)  # {(hash, type, int): [pathlib.Path]}
-    if json_tree_path:
-        tree = load_json_tree(json_tree_path)
-    else:
-        tree = dict()  # {pathlib.Path: (hash, type, int)}
+    if not exclusion:
+        exclusion = []
+
+    listing = collections.defaultdict(set)
+    tree = dict()
+
     _recursive_crawl(path, listing, tree, exclusion)
+
     return listing, tree
 
 
 def _recursive_crawl(path, listing, tree, exclusion):
-    for each in path.iterdir():
-        if each not in exclusion:
-            if each.is_dir():
-                _recursive_crawl(each, listing, tree, exclusion)
-                dir_content_size = 0
-                dir_content_hash_list = []
-                for each_child in each.iterdir():
-                    dir_content_size += tree[each_child][2]
-                    dir_content_hash_list.append(tree[each_child][0])
-                dir_content = '\n'.join(sorted(dir_content_hash_list))
-                dir_content_hash = hashlib.md5(dir_content.encode()).hexdigest()
-                dir_content_key = (dir_content_hash, DIR_TYPE, dir_content_size)
-                listing[dir_content_key].append(each)
-                tree[each] = dir_content_key
-            elif each.is_file():
-                file_hasher = hashlib.md5()
-                with open(each, 'rb') as file_content:
-                    content_stream = file_content.read(BLOCK_SIZE)
-                    while len(content_stream) > 0:
-                        file_hasher.update(content_stream)
-                        content_stream = file_content.read(BLOCK_SIZE)
-                file_content_hash = file_hasher.hexdigest()
-                file_content_size = each.stat().st_size
-                file_content_key = (file_content_hash, FILE_TYPE, file_content_size)
-                listing[file_content_key].append(each)
-                tree[each] = file_content_key
+    if path.is_dir():
+        dir_content_size = 0
+        dir_content_hash_list = []
+        for each_child in path.iterdir():
+            if each_child.name not in exclusion:
+                _recursive_crawl(each_child, listing, tree, exclusion)
+                dir_content_size += tree[each_child][2]
+                dir_content_hash_list.append(tree[each_child][0])
+        dir_content = '\n'.join(sorted(dir_content_hash_list))
+        dir_content_hash = hashlib.md5(dir_content.encode()).hexdigest()
+        dir_content_key = (dir_content_hash, DIR_TYPE, dir_content_size)
+        listing[dir_content_key].add(path)
+        tree[path] = dir_content_key
+    elif path.is_file():
+        file_hasher = hashlib.md5()
+        with open(path, 'rb') as file_content:
+            content_stream = file_content.read(BLOCK_SIZE)
+            while len(content_stream) > 0:
+                file_hasher.update(content_stream)
+                content_stream = file_content.read(BLOCK_SIZE)
+        file_content_hash = file_hasher.hexdigest()
+        file_content_size = path.stat().st_size
+        file_content_key = (file_content_hash, FILE_TYPE, file_content_size)
+        listing[file_content_key].add(path)
+        tree[path] = file_content_key
+
+
+def unify(listings, trees):
+    tree = dict()  # = {pathlib.Path: (hash, type, int)}
+    for each_tree in trees:
+        for k, v in each_tree.items():
+            if (not k in tree) or (tree[k][2] < v[2]):
+                tree[k] = v
+    listing = collections.defaultdict(set)  # = {(hash, type, int): {pathlib.Path}}
+    for each_listing in listings:
+        for k, v in each_listing.items():
+            for each_v in v:
+                if tree[each_v] == k:
+                    listing[k].add(each_v)
+    return listing, tree
 
 
 def dump_json_listing(listing, file_path):
-    serializable_listing = {str(tuple_key): [str(path) for path in path_list]
-                            for tuple_key, path_list in listing.items()}
+    """
+    :param: listing to serialize in json
+    :rtype: collections.defaultdict(set) = {(hash, type, int): {pathlib.Path}}
+
+    :param file_path: path to create the json serialized listing
+    :type file_path: pathlib.Path
+    """
+    serializable_listing = {str(tuple_key): [str(path) for path in path_set]
+                            for tuple_key, path_set in listing.items()}
     json_listing = json.dumps(serializable_listing)
     file_path.write_text(json_listing)
 
 
 def load_json_listing(file_path):
+    """
+    :param file_path: path to an existing json serialized listing
+    :type file_path: pathlib.Path
+
+    :return: deserialized listing
+    :rtype: collections.defaultdict(set) = {(hash, type, int): {pathlib.Path}}
+    """
     json_listing = file_path.read_text()
     serializable_listing = json.loads(json_listing)
-    dict_listing = {ast.literal_eval(tuple_key): [pathlib.Path(path) for path in path_list]
+    dict_listing = {ast.literal_eval(tuple_key): {pathlib.Path(path) for path in path_list}
                     for tuple_key, path_list in serializable_listing.items()}
-    listing = collections.defaultdict(list, dict_listing)
+    listing = collections.defaultdict(set, dict_listing)
     return listing
 
 
 def dump_json_tree(tree, file_path):
+    """
+    :param: tree to serialize in json
+    :rtype: dict = {pathlib.Path: (hash, type, int)}
+
+    :param file_path: path to create the json serialized tree
+    :type file_path: pathlib.Path
+    """
     serializable_tree = {str(path_key): tuple_value for path_key, tuple_value in tree.items()}
     json_tree = json.dumps(serializable_tree)
     file_path.write_text(json_tree)
 
 
 def load_json_tree(file_path):
+    """
+    :param file_path: path to an existing json serialized tree
+    :type file_path: pathlib.Path
+
+    :return: deserialized tree
+    :rtype: dict = {pathlib.Path: (hash, type, int)}
+    """
     json_tree = file_path.read_text()
     serializable_tree = json.loads(json_tree)
     tree = {pathlib.Path(path_key): tuple(value) for path_key, value in serializable_tree.items()}
@@ -104,12 +142,7 @@ def load_json_tree(file_path):
 
 
 if __name__ == '__main__':
-    project_path = pathlib.Path(__file__).resolve().expanduser().parent.parent
-    tests_data_path = project_path / 'tests' / 'data'
-
-## todo   listing, hashes, sizes = main(project_path.parent) crashes on .git
-## todo   replace os.walk by a personal recursion to avoid certain files/directory (like .gitignore)
-
-    listing2, tree2 = crawl(tests_data_path / 'Folder0')
-    dump_json_listing(listing2, tests_data_path / 'Folder0_listing.json')
-    dump_json_tree(tree2, tests_data_path / 'Folder0_tree.json')
+    desktop_path = pathlib.Path('C:/Users') / 'Henri-Olivier' / 'Desktop'
+    listing, tree = crawl(desktop_path, exclusion=['.git', 'DJI', 'huawei', 'huaweiIleDeRe'])
+    dump_json_listing(listing, desktop_path / 'desktop_listing.json')
+    dump_json_tree(tree, desktop_path / 'desktop_tree.json')
--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -5,10 +5,80 @@ import fs_crawler.crawl as fsc
 tests_data_path = pathlib.Path(__file__).resolve().expanduser().parent / 'data'
 
 
+# todo parametrize for Folder3 and co
 def test_crawl():
+    # run
     listing, tree = fsc.crawl(tests_data_path / 'Folder0')
 
+    # for logging purpose only
+    fsc.dump_json_listing(listing, tests_data_path / 'Folder0_listing.json')
+    fsc.dump_json_tree(tree, tests_data_path / 'Folder0_tree.json')
+
+    # load expected
     expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_expected.json')
-    assert expected_listing == listing
     expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_expected.json')
+
+    # verify
+    assert expected_listing == listing
     assert expected_tree == tree
+
+
+def test_crawl_with_exclusions():
+    # run
+    listing, tree = fsc.crawl(tests_data_path / 'Folder0',
+                              exclusion=['Folder3', 'Folder4_1', 'file3.txt', 'groundhog.png'])
+
+    # for logging purpose only
+    fsc.dump_json_listing(listing, tests_data_path / 'Folder0_listing_with_exclusions.json')
+    fsc.dump_json_tree(tree, tests_data_path / 'Folder0_tree_with_exclusions.json')
+
+    # load expected
+    expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_with_exclusions_expected.json')
+    expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_with_exclusions_expected.json')
+
+    # verify
+    assert expected_listing == listing
+    assert expected_tree == tree
+
+
+def test_crawl_with_exclusions2():
+    # run
+    listing0_no3, tree0_no3 = fsc.crawl(tests_data_path / 'Folder0', exclusion=['Folder3'])
+    listing3, tree3 = fsc.crawl(tests_data_path / 'Folder0' / 'Folder3')
+    listing, tree = fsc.unify([listing0_no3, listing3], [tree0_no3, tree3])
+    listing0_full, tree0_full = fsc.crawl(tests_data_path / 'Folder0')
+
+    # for logging purpose only
+    fsc.dump_json_listing(listing, tests_data_path / 'listing.json')
+    fsc.dump_json_listing(listing0_full, tests_data_path / 'listing0_full.json')
+    fsc.dump_json_tree(tree, tests_data_path / 'tree.json')
+    fsc.dump_json_tree(tree0_full, tests_data_path / 'tree0_full.json')
+
+    # verify
+#    assert listing == listing0_full
+    assert tree == tree0_full
+
+
+def test_crawl_asynchronous():
+    existing_listing_path = tests_data_path / 'Folder0_listing_with_exclusions_expected.json'
+    existing_tree_path = tests_data_path / 'Folder0_tree_with_exclusions_expected.json'
+    listing, tree = fsc.crawl(tests_data_path / 'Folder0',
+                              json_listing_path=existing_listing_path,
+                              json_tree_path=existing_tree_path)
+
+    expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_with_exclusions_expected.json')
+    expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_with_exclusions_expected.json')
+
+    listings = [listing, listing, listing, expected_listing]
+    trees = [expected_tree, tree]
+
+    l, t = fsc.unify(listings, trees)
+    el = fsc.load_json_listing(tests_data_path / 'Folder0_listing_expected.json')
+    assert el == l
+    et = fsc.load_json_tree(tests_data_path / 'Folder0_tree_expected.json')
+    assert et == t
+
+    expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_expected.json')
+#    assert sorted(expected_listing) == sorted(listing)
+    expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_expected.json')
+    assert sorted(expected_tree) == sorted(tree)
