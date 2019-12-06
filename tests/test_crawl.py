2fc1c3cb2e65fd9c5277e1829131c1723015e3bc
Henri-Olivier Duché <hoduche@yahoo.fr>
2019-12-06 22:59:12 +0100

    add test unify

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -1,22 +1,31 @@
 import pathlib
 
+import pytest
+
 import fs_crawler.crawl as fsc
+
+debug = False
 
 tests_data_path = pathlib.Path(__file__).resolve().expanduser().parent / 'data'
 
+folders = [('Folder0', 'Folder0'),
+           ('Folder0/Folder3', 'Folder3')]
+names = [each[1] for each in folders]
 
-# todo parametrize for Folder3 and co
-def test_crawl():
+
+@pytest.mark.parametrize('folder, name', folders, ids=names)
+def test_crawl(folder, name):
     # run
-    listing, tree = fsc.crawl(tests_data_path / 'Folder0')
+    listing, tree = fsc.crawl(tests_data_path / folder)
 
     # for logging purpose only
-    fsc.dump_json_listing(listing, tests_data_path / 'Folder0_listing.json')
-    fsc.dump_json_tree(tree, tests_data_path / 'Folder0_tree.json')
+    if debug:
+        fsc.dump_json_listing(listing, tests_data_path / (name + '_listing.json'))
+        fsc.dump_json_tree(tree, tests_data_path / (name + '_tree.json'))
 
     # load expected
-    expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_expected.json')
-    expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_expected.json')
+    expected_listing = fsc.load_json_listing(tests_data_path / (name + '_listing_expected.json'))
+    expected_tree = fsc.load_json_tree(tests_data_path / (name + '_tree_expected.json'))
 
     # verify
     assert expected_listing == listing
@@ -29,8 +38,9 @@ def test_crawl_with_exclusions():
                               exclusion=['Folder3', 'Folder4_1', 'file3.txt', 'groundhog.png'])
 
     # for logging purpose only
-    fsc.dump_json_listing(listing, tests_data_path / 'Folder0_listing_with_exclusions.json')
-    fsc.dump_json_tree(tree, tests_data_path / 'Folder0_tree_with_exclusions.json')
+    if debug:
+        fsc.dump_json_listing(listing, tests_data_path / 'Folder0_listing_with_exclusions.json')
+        fsc.dump_json_tree(tree, tests_data_path / 'Folder0_tree_with_exclusions.json')
 
     # load expected
     expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_with_exclusions_expected.json')
@@ -41,44 +51,17 @@ def test_crawl_with_exclusions():
     assert expected_tree == tree
 
 
-def test_crawl_with_exclusions2():
+def test_unify():
     # run
     listing0_no3, tree0_no3 = fsc.crawl(tests_data_path / 'Folder0', exclusion=['Folder3'])
     listing3, tree3 = fsc.crawl(tests_data_path / 'Folder0' / 'Folder3')
     listing, tree = fsc.unify([listing0_no3, listing3], [tree0_no3, tree3])
     listing0_full, tree0_full = fsc.crawl(tests_data_path / 'Folder0')
 
-    # for logging purpose only
-    fsc.dump_json_listing(listing, tests_data_path / 'listing.json')
-    fsc.dump_json_listing(listing0_full, tests_data_path / 'listing0_full.json')
-    fsc.dump_json_tree(tree, tests_data_path / 'tree.json')
-    fsc.dump_json_tree(tree0_full, tests_data_path / 'tree0_full.json')
-
     # verify
-#    assert listing == listing0_full
+    listing.pop(('7e472b2b54ba97314c63988db267d125', 'DIR', 2698920))
+    listing0_full.pop(('4f8c48630a797715e8b86466e0218aa1', 'DIR', 3598557))
+    assert listing == listing0_full
+    tree.pop(tests_data_path / 'Folder0')
+    tree0_full.pop(tests_data_path / 'Folder0')
     assert tree == tree0_full
-
-
-def test_crawl_asynchronous():
-    existing_listing_path = tests_data_path / 'Folder0_listing_with_exclusions_expected.json'
-    existing_tree_path = tests_data_path / 'Folder0_tree_with_exclusions_expected.json'
-    listing, tree = fsc.crawl(tests_data_path / 'Folder0',
-                              json_listing_path=existing_listing_path,
-                              json_tree_path=existing_tree_path)
-
-    expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_with_exclusions_expected.json')
-    expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_with_exclusions_expected.json')
-
-    listings = [listing, listing, listing, expected_listing]
-    trees = [expected_tree, tree]
-
-    l, t = fsc.unify(listings, trees)
-    el = fsc.load_json_listing(tests_data_path / 'Folder0_listing_expected.json')
-    assert el == l
-    et = fsc.load_json_tree(tests_data_path / 'Folder0_tree_expected.json')
-    assert et == t
-
-    expected_listing = fsc.load_json_listing(tests_data_path / 'Folder0_listing_expected.json')
-#    assert sorted(expected_listing) == sorted(listing)
-    expected_tree = fsc.load_json_tree(tests_data_path / 'Folder0_tree_expected.json')
-    assert sorted(expected_tree) == sorted(tree)
