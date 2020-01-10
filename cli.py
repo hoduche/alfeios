888f1ed76b3359b41d414489dd784f19139f2f4a
Henri-Olivier Duché <hoduche@yahoo.fr>
2020-01-10 18:06:55 +0100

    work on cli

--- cli.py
+++ cli.py
@@ -31,29 +31,42 @@ def main():
     )
 
     # create the parser for the index command
-    index_help = 'Index content of a directory'
+    index_help = 'Index content of a root directory'
     parser_i = subparsers_factory.add_parser(
         name='index',
         aliases=['idx', 'i'],
         help=index_help,  # for alfeios help
-        description=index_help,  # for command help
+        description=index_help + ''':
+  - Index all file and directory contents in a root directory
+    including the inside of zip, tar, gztar, bztar and xztar compressed files
+  - Contents are identified by their hash-code, type (file or directory) and size
+  - It saves in the root directory:
+     - A listing.json file that is a dictionary: content -> list of paths
+     - A tree.json.file that is a dictionary: path -> content
+     - A forbidden.json file that lists paths with no access
+  - In case there is no write access to the root directory,
+    the output files are saved in a temp folder of the filesystem with a unique identifier
+''',
         epilog='''example:
-    like
-    example 1
-    example 2
-    ''',
+  alfeios index
+  alfeios index D:/Pictures
+''',
         formatter_class=argparse.RawTextHelpFormatter
     )
-    parser_i.add_argument('path', help='directory path')
+    parser_i.add_argument(
+        'path',
+        nargs='?', default='.',
+        help='path to the root directory - default is here'
+    )
     parser_i.set_defaults(function_to_call=index)
 
     # create the parser for the duplicate command
-    duplicate_help = 'Find duplicate content in a directory'
+    duplicate_help = 'Find duplicate content in a root directory'
     parser_d = subparsers_factory.add_parser(
         name='duplicate',
         aliases=['dup', 'd'],
         help=duplicate_help,  # for alfeios help
-        description=duplicate_help + '''
+        description=duplicate_help + ''':
   - List all duplicated files and directories in a root directory
   - Save the duplicate listing as a duplicate_listing.json file in the root directory
   - Print the potential space gain
@@ -73,18 +86,20 @@ def main():
     parser_d.add_argument(
         'path',
         nargs='?', default='.',
-        help='path to the root directory (or listing.json) - default is here')
+        help='path to the root directory (or listing.json) - default is here'
+    )
     parser_d.add_argument(
         '-s', '--save-listing', action='store_true',
-        help='save complete listing and tree (deactivated by default)')
+        help='save complete listing and tree (deactivated by default)'
+    )
     parser_d.set_defaults(function_to_call=duplicate)
 
     # create the parser for the missing command
-    missing_help = 'Find missing content in a new directory from an old directory'
+    missing_help = 'Find missing content in a new root directory from an old root directory'
     parser_m = subparsers_factory.add_parser(
         name='missing',
         aliases=['mis', 'm'],
-        help=missing_help,
+        help=missing_help,  # for alfeios help
         description=missing_help + ''':
   - List all files and directories that are present in an old root directory
     and that are missing in a new one
@@ -103,13 +118,16 @@ def main():
     )
     parser_m.add_argument(
         'old_path',
-        help='path to the old root directory (or old listing.json)')
+        help='path to the old root directory (or old listing.json)'
+    )
     parser_m.add_argument(
         'new_path',
-        help='path to the new root directory (or new listing.json)')
+        help='path to the new root directory (or new listing.json)'
+    )
     parser_m.add_argument(
         '-s', '--save-listing', action='store_true',
-        help='save complete listing and tree (deactivated by default)')
+        help='save complete listing and tree (deactivated by default)'
+    )
     parser_m.set_defaults(function_to_call=missing)
 
     # parse command line and call appropriate function
